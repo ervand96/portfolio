3df86846c924c9e3445b276bd6b496de98965f39
ervand96 <ervandharutyunyan06096@gmail.com>
2022-09-29 13:00:57 -0700

    change

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import HeroSection from "./pages/hero-section";
+import Home from "./pages/home";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <HeroSection /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/featured/header/index.tsx
@@ -0,0 +1,10 @@
+import HeaderSection from "../../shared/header-section";
+
+
+export default function Header() {
+    return(
+        <div>
+            <HeaderSection />
+        </div>
+    )
+}
